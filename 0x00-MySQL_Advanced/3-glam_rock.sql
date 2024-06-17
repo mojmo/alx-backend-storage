7e7d41e211df3d8b58723159cb766fa99da62329
mojtaba <mojtaba.mohamed187@gmail.com>
2024-06-18 00:06:41 +0300

    use like to match more cases

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,6 +3,6 @@
 SELECT band_name AS band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 -- Filter bands with 'Glam Rock' style
-WHERE style = 'Glam Rock'
+WHERE style LIKE '%Glam rock%'
 -- Order by lifespan (descending)
 ORDER BY lifespan DESC;
