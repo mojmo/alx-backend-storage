94671f371dab1fb73f7c33b30bf002b953fbd04d
mojtaba <mojtaba.mohamed187@gmail.com>
2024-06-17 23:25:29 +0300

    sum fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- This script ranks country origins of bands based on the number of non-unique fans.
 
 -- Count fans for each origin (non-unique)
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 -- Source table
 FROM metal_bands
 -- Group rows by origin
