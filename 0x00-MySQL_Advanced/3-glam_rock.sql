eb053999278bf7383bbe8d852c57dd3ad2a5e716
mojtaba <mojtaba.mohamed187@gmail.com>
2024-06-18 00:05:31 +0300

    Add task '3. Old school band'

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- This script lists Glam Rock bands ranked by their longevity (years active).
+
+SELECT band_name AS band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+-- Filter bands with 'Glam Rock' style
+WHERE style = 'Glam Rock'
+-- Order by lifespan (descending)
+ORDER BY lifespan DESC;
